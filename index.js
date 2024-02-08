30b5b37d5a56f65df53030322b83ddcb6c6d518a
Akshaya Ashok <akshayaashok1109@gmail.com>
2024-02-08 11:10:20 +0530

    Initial changes made

---
+++ index.js
@@ -0,0 +1,14 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+function opentab(input)
+{clea
+    for(var tablink of tablinks)
+    {
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabContents)
+    {
+        tabcontent.classList.remove("active-tab");
+    }
+    document.getElementById(input).classList.add('active-tab');
+}
